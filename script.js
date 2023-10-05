a95f582f566bf1e3c719fa5c7867df80064d43c8
unknown <osdavizcaya6@gmail.com>
2023-10-05 09:48:43 -0500

    proyectos

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('calen').addEventListener('click', function () {
+    window.open('Calendario/index.html', '_blank');
+});
+
+document.getElementById('fact').addEventListener('click', function () {
+    window.open('factura/index.html', '_blank');
+});
+
+document.getElementById('log').addEventListener('click', function () {
+    window.open('formulario/index.html', '_blank');
+});
+
+document.getElementById('who').addEventListener('click', function () {
+    window.open('pagina_web/index.html', '_blank');
+});
